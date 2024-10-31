bfd0d09abc81014e91523cd9230540fd9877845f
Kai Hatje <kai.hatje@app-akademie.com>
2024-10-31 10:55:16 +0100

    WIP Refactored away more cards.

--- lib/features/movies/widgets/big_movie_carousel.dart
+++ lib/features/movies/widgets/big_movie_carousel.dart
@@ -22,7 +22,15 @@ class BigMovieCarousel extends StatelessWidget {
       movieTime: '18:00 - 21:13',
       movieDescription:
           "Herr der Ringe ist ein Film, der auf dem gleichnamigen Buch von J.R.R. Tolkien basiert. Der Film wurde von Peter Jackson inszeniert. Der Film wurde in Neuseeland gedreht.",
-    )
+    ),
+    MovieData(
+      imageUrl:
+          'https://images.cgames.de/images/gsgp/290/harry-potter-filme_6229276.jpg',
+      movieTitle: 'Harry Potter',
+      movieTime: '10:00 - 12:30',
+      movieDescription:
+          "Harry Potter ist eine Fantasy-Romanreihe der britischen Schriftstellerin Joanne K. Rowling. Erzählt wird die Geschichte des jungen Zauberers Harry Potter, der als Baby von der bösen Hexe Lord Voldemort ermordet werden sollte, aber überlebte und seither gegen ihn kämpft.",
+    ),
   ];
 
   // final MovieData startWarsMovieData = MovieData(
@@ -57,91 +65,8 @@ class BigMovieCarousel extends StatelessWidget {
 
         MovieCard(movieData: movies[0]),
         MovieCard(movieData: movies[1]),
-        SizedBox(
-          height: 450,
-          width: 350,
-          child: Card(
-            elevation: 8.0,
-            margin: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: Image.network(
-                    'https://images.cgames.de/images/gsgp/290/harry-potter-filme_6229276.jpg',
-                    height: 150,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.movie, size: 40.0),
-                          const SizedBox(width: 16.0),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Harry Potter',
-                                style:
-                                    Theme.of(context).textTheme.headlineMedium,
-                              ),
-                              Text(
-                                '10:00 - 12:30',
-                                style: Theme.of(context).textTheme.labelLarge,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 16.0),
-                      const Text(
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        "Harry Potter ist eine Fantasy-Romanreihe der britischen Schriftstellerin Joanne K. Rowling. Erzählt wird die Geschichte des jungen Zauberers Harry Potter, der als Baby von der bösen Hexe Lord Voldemort ermordet werden sollte, aber überlebte und seither gegen ihn kämpft.",
-                      ),
-                      const SizedBox(height: 16.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          FilledButton(
-                            onPressed: () {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Ticket zu Harry Potter'),
-                                ),
-                              );
-                            },
-                            child: const Text('Ticket kaufen'),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          OutlinedButton(
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (_) =>
-                                      const HarryPotterDetailsScreen(),
-                                ),
-                              );
-                            },
-                            child: const Text('Vorschau'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+        MovieCard(movieData: movies[2]),
+
         SizedBox(
           height: 450,
           width: 350,
